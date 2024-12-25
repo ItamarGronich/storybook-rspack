1a961437dc61b8fdde5a3a964c97568646bb3748
ItamarGronich <itamar.gronich@fiverr.com>
2024-12-25 14:46:57 +0200

    fix bad merge

--- packages/storybook-framework-react-rspack/eslint.config.js
+++ packages/storybook-framework-react-rspack/eslint.config.js
@@ -8,38 +8,8 @@ module.exports = [
       '@nx/dependency-checks': [
         'error',
         {
-<<<<<<< Updated upstream
-          ignoredDependencies: [
-            '@gitamar/storybook-builder-rspack',
-            '@gitamar/storybook-preset-react-rspack',
-            '@storybook/react',
-            '@babel/core',
-            'react-dom',
-            'storybook',
-            'typescript',
-            'react',
-          ],
-          ignoredFiles: [
-            '{projectRoot}/eslint.config.{js,cjs,mjs}',
-            '{projectRoot}/vite.config.{js,ts,mjs,mts}',
-          ],
-||||||| Stash base
-          ignoredDependencies: [
-            '@storybook/react',
-            '@babel/core',
-            'react-dom',
-            'storybook',
-            'typescript',
-            'react',
-          ],
-          ignoredFiles: [
-            '{projectRoot}/eslint.config.{js,cjs,mjs}',
-            '{projectRoot}/vite.config.{js,ts,mjs,mts}',
-          ],
-=======
           ignoredDependencies: ['@gitamar/storybook-preset-react-rspack', '@storybook/react', '@babel/core', 'react-dom', 'storybook', 'typescript', 'react'],
           ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}', '{projectRoot}/vite.config.{js,ts,mjs,mts}'],
->>>>>>> Stashed changes
         },
       ],
     },
